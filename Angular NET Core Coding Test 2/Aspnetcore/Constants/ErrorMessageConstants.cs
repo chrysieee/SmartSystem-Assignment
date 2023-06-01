0cf0cdc635c91f8daa14c3267c3d1b0b38e11950
Chrys <chrysieee@gmail.com>
2023-06-01 22:59:39 +0800

    Push codes

---
+++ Angular NET Core Coding Test 2/Aspnetcore/Constants/ErrorMessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Constants
+{
+    public class ErrorMessageConstants
+    {
+        public const string INVALID_PAGE_NUMBER = "Invalid Page Number";
+        public const string INVALID_ROWS_OF_PAGE = "Invalid Rows Of Page";
+    }
+}
---
+++ Angular NET Core Coding Test 2/Aspnetcore/Constants/SqlConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApi.Constants
+{
+    public class SqlConstants
+    {
+        public const string GET_SENT_FOLDER = @"
+            SELECT
+                m.Id, 
+                su.StaffName as SendingStaffName, 
+                su.StaffEmail as SendingStaffEmail, 
+                ru.StaffName as ReceivingStaffName, 
+                ru.StaffEmail as ReceivingStaffEmail, 
+                m.Subject, 
+                m.Message, 
+                m.SentTime, 
+                m.SentSuccessToSMTPServer, 
+                m.[Read],
+                m.Starred, 
+                m.Important, 
+                m.HasAttachments, 
+                m.[Label], 
+                m.Folder
+            FROM Mail m
+            LEFT JOIN Users su ON su.UserID = m.SendingUserID
+            LEFT JOIN Users ru ON ru.UserID = m.ReceivingUserID
+            WHERE 
+                m.SendingUserID = @UserID AND 
+                m.Folder = @Folder
+            ORDER BY m.SentTime DESC;";
+    }
+}
